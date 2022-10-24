4f309f69b02d7e355b623aa5792fedb0087b84f5
isaac111234 <113899487+isaac111234@users.noreply.github.com>
2022-10-24 03:19:04 -0900

    Add files via upload

---
+++ quiztext/index.js
@@ -0,0 +1,17 @@
+function result(){
+    //body......
+    var score = 0;
+    if (document.getElementById('correct1').checked){
+        score++;
+    }
+    if (document.getElementById('correct2').checked){
+        score++;
+    }
+    if (document.getElementById('correct3').checked){
+        score++;
+    }
+    if (document.getElementById('correct4').checked){
+        score++;
+    }
+    document.write("your score is: " +score);
+}
